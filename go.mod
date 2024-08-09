fdcaa8edc97c6671ff5914729149708e624bb51e
Nawaraj <shah.nawaraj.ns@gmail.com>
2024-08-09 10:58:16 +0545

    created env file for security purpose

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	golang.org/x/net v0.25.0 // indirect
 	golang.org/x/sys v0.20.0 // indirect
 	golang.org/x/text v0.15.0 // indirect
--- server/pkg/db/db.go
+++ server/pkg/db/db.go
@@ -4,17 +4,36 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 func Connect() *sql.DB {
-	dsn := "nawaraj:nawaraj100@tcp(localhost:3306)/post_server" // replace with your database credentials
+	// Load environment variables from .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	// Get database credentials from environment variables
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	name := os.Getenv("DB_NAME")
+
+	// Create the Data Source Name (DSN)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, password, host, port, name)
+
+	// Connect to the database
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		log.Fatalf("Error connecting to database: %v", err)
 	}
 
+	// Ping the database to verify the connection
 	err = db.Ping()
 	if err != nil {
 		log.Fatalf("Error pinging database: %v", err)
