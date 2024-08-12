4e91544f829570592a46ecdf21e1015b5d5803d5
Nawaraj <shah.nawaraj.ns@gmail.com>
2024-08-12 11:27:41 +0545

    merged

--- post-service/cmd/main.go
+++ post-service/cmd/main.go
@@ -18,21 +18,13 @@ func main() {
 	database := db.Connect()
 	defer database.Close()
 
-<<<<<<<< HEAD:post-service/cmd/main.go
 	// Initialize the repository
-========
-	// Create the PostRepository instance
->>>>>>>> 64d27f067b21c91a8acc82b612efb223375b5709:post-service/main.go
 	postRepo := repo.NewPostRepository(database)
 
 	// Create a new gRPC server
 	grpcServer := grpc.NewServer()
 
-<<<<<<<< HEAD:post-service/cmd/main.go
 	// Register the PostServiceServer
-========
-	// Register the PostServiceServer using the repository
->>>>>>>> 64d27f067b21c91a8acc82b612efb223375b5709:post-service/main.go
 	postService := service.NewPostServiceServer(postRepo)
 	pb.RegisterPostServiceServer(grpcServer, postService)
 
